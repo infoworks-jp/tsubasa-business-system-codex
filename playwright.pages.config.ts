66275d3ceac9e405634d77dc5e5aa163804b12e0
infoworks-jp <youjikaku@gmail.com>
2026-08-02 13:42:02 +0900

    Rev.2: connect Rev.1 UI to canonical aggregation

--- playwright.pages.config.ts
+++ playwright.pages.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests/e2e',
   testMatch: 'pages-rev2.spec.ts',
-  use: { baseURL: 'http://127.0.0.1:4173', viewport: { width: 1440, height: 1000 } },
+  use: { baseURL: 'http://127.0.0.1:4173' },
+  projects: [
+    { name: 'pc', use: { viewport: { width: 1440, height: 1000 } } },
+    { name: 'smartphone', use: { viewport: { width: 390, height: 844 }, isMobile: true } },
+  ],
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }], ['list']],
   outputDir: 'test-results/pages-rev2',
 });
